894288bc62a84b525a2070cbddacbed3d4c200fd
Justin Rassier <justin.rassier@infinitecampus.com>
2019-12-09 20:20:26 -0600

    more cleanup and adding post CSS

--- src/components/wrapper.tsx
+++ src/components/wrapper.tsx
@@ -5,7 +5,7 @@ export const Wrapper = ({ children }) => (
   <div className="bg-gray-100">
     <Header></Header>
     <div className="min-h-screen flex mt-16 mx-8 flex justify-center">
-      <div className="max-w-2xl">{children}</div>
+      <div className="max-w-2xl post">{children}</div>
     </div>
   </div>
 );
--- src/posts/welcome.md
+++ src/posts/welcome.md
@@ -4,4 +4,11 @@ date: "2019-12-09"
 tags: []
 ---
 
-# Welcome!
+# Welcome to my Blog!
+
+My name is Justin. I am going to write more, but for now I am just testing this out.
+
+Some extra text
+
+- a list
+- a second item
