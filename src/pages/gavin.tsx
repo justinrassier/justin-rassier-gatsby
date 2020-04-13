51d12de32bdc0fda264052e00a7510e69443a041
Justin Rassier <justin.rassier@infinitecampus.com>
2020-04-13 15:49:25 -0500

    silly thing for gavin

---
+++ src/pages/gavin.tsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+export default () => {
+  const numbers = [14, 5, 18, 4, 19];
+  const [output, setOutput] = useState("");
+  let doButtonStuff = () => {
+    setOutput(numbers.join(","));
+  };
+  return (
+    <div className="">
+      <div className="flex justify-center">
+        <button
+          className="bg-red-600 rounded text-gray-100 p-4"
+          onClick={doButtonStuff}
+        >
+          Don't press me
+        </button>
+      </div>
+      <div className="flex justify-center">
+        <div className="text-6xl">{output}</div>
+      </div>
+    </div>
+  );
+};
